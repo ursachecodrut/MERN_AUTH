8de9b3f5c77b3bbab1f34d3eef224c791fbd3573
ursachecodrut <ursache.codrut71@gmail.com>
2021-09-15 14:12:23 +0300

    initial routes & controllers setup

---
+++ routes/auth.route.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const {
+	register,
+	login,
+	forgotPassword,
+	resetPassword,
+} = require('../controllers/auth.controller');
+
+router.route('/register').post(register);
+
+router.route('/login').post(login);
+
+router.route('/forgotPassword').post(forgotPassword);
+
+router.route('/resetPassword/:resetToken').put(resetPassword);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,8 +5,15 @@ dotenv.config({ path: './config.env' });
 
 const app = express();
 
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', require('./routes/auth.route'));
+
+// Server settings
 const PORT = process.env.PORT;
 
+// Server start
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
