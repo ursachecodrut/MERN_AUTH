c0a18047e2537aaa4289ffe8c471a0d2ee72218d
ursachecodrut <ursache.codrut71@gmail.com>
2021-09-16 17:04:35 +0300

    - react file cleanup - add private route in react - create screen components for login, register & privateScreen

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Routing
+import PrivateRoute from './components/routing/PrivateRoute';
+
+// Screens
+import PrivateRoute from './components/screens/PrivateScreen';
+import LoginRoute from './components/screens/LoginScreen';
+import RegisterRoute from './components/screens/RegisterScreen';
+
+const App = () => {
+	return (
+		<Router>
+			<div className="app">
+				<Switch>
+					<PrivateRoute exact path="/" component={PrivateScreen} />
+					<Route exact path="/login" component={LoginScreen} />
+					<Route exact path="/register" component={RegisterScreen} />
+				</Switch>
+			</div>
+		</Router>
+	);
+};
 
 export default App;
